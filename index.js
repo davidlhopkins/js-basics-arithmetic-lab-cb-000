6df2df13cc2d0231a3647e62ccd9c95a7593aa9e
David Hopkins <dlhopkins2@gmail.com>
2018-07-04 23:22:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let ageIsValid = Number.isInteger(currentAge);
 
 const newID = oldID + 1000000000;
 
-let randomNumber = Math.floor(Math.random() * 20) + 1;
+let randomNumber = Math.random() * 20 + 1;
 
 let randomInteger = Math.floor(randomNumber);
