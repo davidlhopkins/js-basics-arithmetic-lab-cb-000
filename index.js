a9453df2c5c2a6250dcf354e0cb03a62a31489ef
David Hopkins <dlhopkins2@gmail.com>
2018-07-04 23:31:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ let randomNumber = Math.random() * 20 + 1;
 let randomInteger = Math.floor(randomNumber);
 
 let randomUserID = 1000000001
-    randomUserid = Math.floor(Math.random() * 20)
+    randomUserid = Math.random() * 20
